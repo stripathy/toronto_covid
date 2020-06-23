a7f7e758efb7c81c9f27848b87ff97d89a4fa115
Shreejoy Tripathy <stripat3@gmail.com>
2020-06-23 10:47:01 -0400

    adding low income percent graph

--- scratch.R
+++ scratch.R
@@ -12,16 +12,17 @@ library(ggrepel)
 neighbourhood_profiles = read.csv(url('https://ckan0.cf.opendata.inter.prod-toronto.ca/download_resource/ef0239b1-832b-4d0b-a1f3-4153e53b189e?format=csv'))
 
 
-neighbourhood_profiles %>% filter(Characteristic == ' Average after-tax income of households in 2015 ($)') 
+neighbourhood_profiles %>% filter(Characteristic == 'Prevalence of low income based on the Low-income cut-offs, after tax (LICO-AT) (%)') 
 
 
-avg_income_vector = neighbourhood_profiles %>% filter(X_id == 1030)
+avg_income_vector = neighbourhood_profiles %>% filter(X_id == 1030 | Characteristic == 'Prevalence of low income based on the Low-income cut-offs, after tax (LICO-AT) (%)')
 
 avg_incomes_only = avg_income_vector[, 6:ncol(avg_income_vector)] 
 
 avg_incomes_transposed = avg_incomes_only %>% t() %>% as.data.frame() %>% tibble::rownames_to_column(var = "norm_neighbourhood_name")
-colnames(avg_incomes_transposed) = c("norm_neighbourhood_name", 'avg_income')
+colnames(avg_incomes_transposed) = c("norm_neighbourhood_name", 'avg_income', 'low_income_pct')
 avg_incomes_transposed$avg_income = readr::parse_number(as.character(avg_incomes_transposed$avg_income))
+avg_incomes_transposed$low_income_pct = as.numeric(as.character(avg_incomes_transposed$low_income_pct))
 
 
 covid_data = read_excel('~/Downloads/CityofToronto_COVID-19_NeighbourhoodData.xlsx', sheet = 'All Cases and Rates by Neighbou')
@@ -34,6 +35,10 @@ theme_set(theme_cowplot())
 
 merged_covid_data %>% ggplot(aes(x = avg_income, y = `Rate per 100,000 people`)) + 
   geom_point() + scale_x_log10(name="Average after-tax income of households in 2015 ($)", labels = scales::comma) + 
+  ylab("Covid rate per 100,000 people") + geom_smooth(method = "lm", se = F)
+
+merged_covid_data %>% ggplot(aes(x = low_income_pct, y = `Rate per 100,000 people`)) + 
+  geom_point() + scale_x_log10(name="Prevalence of low income based on the Low-income cut-offs,\n after tax (LICO-AT) (%)", labels = scales::comma) + 
   ylab("Covid rate per 100,000 people") + geom_smooth(method = "lm", se = F)
 
 
